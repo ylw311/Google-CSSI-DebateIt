3c247bf8c68ecdba933d53f44a43ec474b85934d
Linda Wang <61102300+ylw311@users.noreply.github.com>
2022-08-05 13:22:15 +0000

    mvp

--- target.js
+++ target.js
@@ -1,5 +1,7 @@
 let url_string = window.location.href; // www.test.com?filename=test
 let url = new URL(url_string);
+
+
 let param1Value = url.searchParams.get("param1");
 let param2Value = url.searchParams.get("param2");
 if (param1Value === 'android') {
